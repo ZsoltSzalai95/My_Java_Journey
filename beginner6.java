741e193c82e33c686b4d7c56e6867fc4c0d76967
zsozsi9512 <102908557+zsozsi9512@users.noreply.github.com>
2022-04-27 12:43:37 +0200

    Create beginner6.java

---
+++ beginner6.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) {
+//        String s = "Merry";
+//        System.out.println("Christmas");
+//        System.out.println("Merry New");
+//        System.out.print("weekend");
+        System.out.print("Happy New");
+//        System.out.print("weekend");
+//        System.out.println(s);
+//        System.out.print("Merry New!");
+        System.out.print(" ");
+        System.out.println("Year");
+    }
+}
